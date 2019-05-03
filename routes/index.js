39266614804356f0d0ac492287b0a2d331a461dc
shubh <psubham94@gmail.com>
2019-05-03 15:13:48 +0530

    add  bookForm.js

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,20 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/new', (req, res)=> {
+  res.render('bookForm');
+
+  book.find(err,book => {
+    console.log(books)
+  })
+});
+
+router.post('/', (req,res,next) => {
+  Book.create(req.body, (err,books)=>{
+    console.log(err,books);
+    res.redirect('/');
+    if(err) return next(err);
+  })
+})
+
 module.exports = router; 
